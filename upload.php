d0b410ba6958ff133ffa6125d9afe4fad9b445e7
kcouch3 <55259019+kcouch3@users.noreply.github.com>
2020-09-13 12:47:09 -0400

    Update upload.php
    Added code to create directory upload to store images if it does not already exist.

--- upload.php
+++ upload.php
@@ -2,6 +2,9 @@
 $target_dir = "upload/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 
+if (!file_exists($target_dir)) {
+    mkdir($target_dir, 0777, true);
+}
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir.$_FILES['file']['name'])) {
    $status = 1;
 }
