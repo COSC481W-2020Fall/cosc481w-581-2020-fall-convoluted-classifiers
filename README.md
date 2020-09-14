f305a94d9b49bcd8bba3a4d62b4f8e1210040c0a
Jacob Hillock <jacob@hillock.cc>
2020-09-14 16:57:41 -0400

    Got testing detection to work, going to work on accuracy

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 This is an open source dog breed detector.
 
 # Install
-1. [Install python from python.org](https://www.python.org/downloads/)
+1. [Install python3.7 from python.org](https://www.python.org/downloads/)
 2. Clone/download the repository
    - Download zip
    - `git clone https://github.com/COSC481W-2020Fall/cosc481w-581-2020-fall-convoluted-classifiers.git`
 3. Create a venv (using bash) (Windows recommended using git terminal, or ubuntu shell)
-    1. `python -m venv venv`
+    1. `python3.7 -m venv venv`
+       1. if previous does not work on windows, use `py -3.7 -m venv venv`
     2. `pip install -r requirements.txt`
 
 # Prototype Description
--- detect.py
+++ detect.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     
     model_path = "./models/yolo-tiny.h5"
     input_dir = "./.positives/"
-    images = os.scandir(input_dir)
+    images = os.listdir(input_dir)
     output_dir = "./.output/"
     
     detector.setModelTypeAsTinyYOLOv3()
@@ -17,7 +17,13 @@ if __name__ == '__main__':
     dog_detect = detector.CustomObjects(dog=True)
 
     for image in images:
-        detection = detector.detectCustomObjectsFromImage(
-            custom_objects=dog_detect, input_image=f'{input_dir}{image}', output_image_path=f'{output_dir}{image}')
-        for eachItem in detection:
-            print(f'{eachItem["name"]} : {eachItem["percentage_probability"]}')
+        print(image)
+        if image.split('.')[-1].lower() in ['png', 'jpg', 'jpeg']:
+            detection = detector.detectCustomObjectsFromImage(
+                custom_objects=dog_detect,
+                input_image=f'{os.path.join(input_dir, image)}',
+                output_image_path=f'{os.path.join(output_dir, image)}',
+                minimum_percentage_probability=0
+            )
+            for eachItem in detection:
+                print(f'{eachItem["name"]} : {eachItem["percentage_probability"]}')
