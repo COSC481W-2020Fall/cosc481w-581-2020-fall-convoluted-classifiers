4c0b72f691daad2647341dd40a7cb504a1f371fc
kcouch3 <55259019+kcouch3@users.noreply.github.com>
2020-09-14 23:34:52 -0400

    Added additional comments
    New comments for the PHP code

--- upload.php
+++ upload.php
@@ -2,9 +2,11 @@
 $target_dir = "upload/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 
+/* If file to store images does not exist, create it */
 if (!file_exists($target_dir)) {
     mkdir($target_dir, 0777, true);
 }
+/* Store images in file */
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir.$_FILES['file']['name'])) {
    $status = 1;
 }
