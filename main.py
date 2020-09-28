d3408303539235f7e1a2459974ded1a01d9b37a8
Junyao SG <shangguanjunyao@gmail.com>
2020-09-27 23:28:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from detect import *
 import argparse
+import tensorflow as tf
+import os
 
+# slience debugging logs and warning messages or 'deprecated' warning
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# set memory growth equal to True for each GPU
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
 
 def main(args):
     if (args.test_text):
