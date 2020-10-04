a276ce90651ef81ec3cb51dc3b8b84c50de10e6c
cflones <55413825+cflones@users.noreply.github.com>
2020-10-03 20:40:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ if gpus:
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
 
-def main(args):
-    if (args.test_text):
+def main(args):                     #create the main function with input args
+    if (args.test_text):                     #if input is test, just run test
         print(args.test_text)
     
-    detect(args.test_location, args.output_location)
+    detect(args.test_location, args.output_location)                #runs detect program with input and output locations
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_text', default=None,
-                        help='Test text to just print to see if working [default: None]')
+                        help='Test text to just print to see if working [default: None]')           #test to make sure program is working
     parser.add_argument('--test_location', default='.positives/',
-                        help='The location of the image, or directory of images, for testing')
+                        help='The location of the image, or directory of images, for testing')              #input for location of image(s), default is .positives folder
     parser.add_argument('--output_location', default='.output/',
-                        help='The location of the output directory for positve outputs')
+                        help='The location of the output directory for positve outputs')                    #input for output location, default is output folder
 
 
-    args = parser.parse_args()
+    args = parser.parse_args()                  #args input for function is parse module
 
-    main(args)
+    main(args)                  #call main function with args input
