18f032a3d6d314a895762c4e319955e1958684dd
Jacob Hillock <jacob@hillock.cc>
2020-10-14 18:03:34 -0400

    make a way to automatically download, untar, and rename folders from stanfard dataset

--- image_dataset/download_images.py
+++ image_dataset/download_images.py
@@ -1,22 +1,31 @@
-import requests
-from os.path import join
-from os.path import isfile
-import tarfile
+from requests import get
+from os.path import join, isfile, isdir
+from os import listdir
+from os import rename
+from tarfile import open
 
 def download_images () :                  # To download image dataset
-    local_images = join(".", join("image_dataset", "images.tar"))                      # set location
-    if not isfile(local_images):
+    image_loc = join(".", "image_dataset")
+    image_tar = join(image_loc, "images.tar")                   # set location
+    if not isfile(image_tar):
         print("Downloading stanford dog dataset images, this will take a moment...")               # Print message so user doesn't think program isn't working
         url = "http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar"                  # URL for the download
-        r = requests.get(url)
+        r = get(url)
 
-        with open(local_images,'wb') as f:                        # open model and write content into it to run the program
+        with open(image_tar,'wb') as f:                        # open model and write content into it to run the program
             print("writing to file")
             f.write(r.content)
     
-    with open(local_images) as file:
-        tar = tarfile.open(fileobj=file, bufsize=1000000)
-        tar.extractall(members=tar.getmembers())
+    image_folder = join(image_loc, "Images")
+    if not isdir(image_folder):
+        tar = open(image_tar)
+        tar.extractall(image_loc)
+        tar.close()
+
+    dirs = [f for f in listdir(image_folder) if isdir(join(image_folder, f))]
+    for d in dirs:
+        breed = d.split("-", 1)[-1]
+        rename(join(image_folder, d), join(image_folder, breed))
 
 if __name__ == '__main__':
     download_images()
