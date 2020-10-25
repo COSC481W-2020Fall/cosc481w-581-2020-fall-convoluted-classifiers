3cea219684d7ee2c509cca1021e4689d12b2b075
kcouch3 <kcouch3@emich.edu>
2020-10-25 19:56:32 -0400

    Set up Robolectric for unit testing and added two unit tests for the application.

--- CNNapp/app/src/test/java/com/example/cnnapp/UnitTesting.java
+++ CNNapp/app/src/test/java/com/example/cnnapp/UnitTesting.java
@@ -2,44 +2,51 @@ package com.example.cnnapp;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import static androidx.core.app.ActivityCompat.startActivityForResult;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+import static org.robolectric.Shadows.shadowOf;
+import static android.os.Looper.getMainLooper;
 import static org.junit.Assert.*;
-import android.os.Environment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/*
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-*/
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.P)
+public class UnitTesting
+{
+    private MainActivity activity;
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
 
-public class UnitTesting {
+    /* Test to see if application can access the camera */
     @Test
-    public void dispatchTakePictureIntent()
-    {
-        //Opens camera app
+    public void testCameraAccess() {
+        Context context = activity;
+        PackageManager packageManager = context.getPackageManager();
+        assertEquals(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA), false);
     }
 
     /* Test to see if file can be created */
     @Test
-    public void createImageFile() throws IOException
-    {
+    public void createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStorageDirectory();
@@ -54,56 +61,17 @@ public class UnitTesting {
         assertNotNull(imageFile);
     }
 
+    /* Test to see if Second Activity can be opened */
+    /* Not Working - null*/
     @Test
-    public void onActivityResult()
+    public void testOpeningSecondActivity() throws NullPointerException
     {
-        //Saving image to gallery
-
+        shadowOf(getMainLooper()).idle();
+        Intent expectedIntent = new Intent(activity, SecondActivity.class);
+        Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
     }
 }
-
-/**
- * Basic tests showcasing simple view matchers and actions like {@link ViewMatchers#withId},
- * {@link ViewActions#click} and {@link ViewActions#typeText}.
- * <p>
- * Note that there is no need to tell Espresso that a view is in a different {@link Activity}.
- */
-/*
-@RunWith(AndroidJUnit4.class)
-@LargeTest
-public class ChangeTextBehaviorTest {
-
-    public static final String STRING_TO_BE_TYPED = "Espresso";
-
-    /**
-     * Use {@link ActivityScenarioRule} to create and launch the activity under test, and close it
-     * after test completes. This is a replacement for {@link androidx.test.rule.ActivityTestRule}.
-     */
-    //@Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
-    //        = new ActivityScenarioRule<>(MainActivity.class);
-/*
-    @Test
-    public void changeText_sameActivity() {
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput))
-                .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
-        onView(withId(R.id.changeTextBt)).perform(click());
-
-        // Check that the text was changed.
-        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
-    }
-
-    @Test
-    public void changeText_newActivity() {
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput)).perform(typeText(STRING_TO_BE_TYPED),
-                closeSoftKeyboard());
-        onView(withId(R.id.activityChangeTextBtn)).perform(click());
-
-        // This view is in a different Activity, no need to tell Espresso.
-        onView(withId(R.id.show_text_view)).check(matches(withText(STRING_TO_BE_TYPED)));
-    }
-}*/
 
 
 
